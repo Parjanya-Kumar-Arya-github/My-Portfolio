a2ec9d9545dcd6697d7d715e3b61ecf67aa23ba9
Parjanya Kumar Arya <Parjanya789@gmail.com>
2021-07-06 12:29:35 +0530

    Update views.py

--- Profile/views.py
+++ Profile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Contact
 from django.contrib import messages
-from django.core.mail import send_mail
+
 
 # Create your views here.
 def Home(request):
@@ -12,13 +12,6 @@ def Home(request):
         message = request.POST.get('message', '')
         contact = Contact(name=name, email=email, subject=subject, message=message)
         contact.save()
-        send_mail(
-        f'{subject} from {name} email: {email}',
-        f'{message}',
-        'phantomkingpk12@gmail.com',
-        ['Parjanya789@gmail.com'],
-        fail_silently=False,
-        )
         messages.success(request,"Message Sent!")
         return redirect('/')
     return render(request, 'index.html')
