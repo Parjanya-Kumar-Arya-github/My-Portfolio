3b7efd0d61ff179aa516b08c74b3035a4f0f40c7
Parjanya Kumar Arya <Parjanya789@gmail.com>
2021-07-06 12:05:01 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'epqm+&2q7(!cfup=1=y%@iuu_@o5z&ag%fg!-oc52%+1_&ui1='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
